065baff159e24391771241c77d6e76c3d928dfff
Kameron Ashley <kameronashley@gmail.com>
2021-09-13 18:14:24 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names,events) {
+    let thankYouList = [];
+    for (let x = 0; x < names.length; x++) {
+    thankYouList.push(`Thank you, ${names[x]}, for the wonderful surprise gift!`)
+    }
+    return thankYouList;
+    }
+    
+    function countDown() {
+    let countDown = 10
+    while (countDown <= 10,countDown >= 0) {
+    console.log(countDown--)
+    }
+    }
+    
